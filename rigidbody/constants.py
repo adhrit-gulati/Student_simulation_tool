8b1e78e800c0b6e43332550ba4e42ff5063a3ec8
Adhrit Gulati <adhritgulati2@gmail.com>
2026-03-13 12:38:08 +0530

    added the new constants

--- rigidbody/constants.py
+++ rigidbody/constants.py
@@ -8,9 +8,12 @@ meter = 50                  # 50 pixels = 1 meter
 g = np.array([0.0, -9.81])  # m/s^2
 k = 8.98e9                  # N m^2 C^-2
 
-energy_loss_edge = 0.8
-energy_loss_ball = 0.8
+energy_loss_ball_edge = 0.8
+energy_loss_ball_ball = 0.8
+energy_loss_ball_barrier = 0.3
 
 trail_length = 50            # number of points a trail
 barrier_draw_radius = 5
 barrier_remove_radius = 15
+
+number_substeps = 10
